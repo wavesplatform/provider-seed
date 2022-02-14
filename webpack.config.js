cacc9a01195c49474c403229e353f3b183421099
inazarov <inazarov@web3tech.ru>
2022-02-15 02:22:31 +0300

    add commonjs module

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,27 @@
 const { resolve } = require('path');
 
 
-module.exports = [
-    {
-        entry: './src/index.ts',
-        mode: "production",
-        module: {
-            rules: [
-                {
-                    test: /\.tsx?$/,
-                    use: 'ts-loader',
-                    exclude: /node_modules/,
-                },
-            ],
-        },
-        resolve: {
-            extensions: ['.tsx', '.ts', '.js'],
-        },
-        output: {
-            libraryTarget: 'umd',
-            globalObject: 'this',
-            library: 'providerSeed',
-            filename: 'provider-seed.js',
-            path: resolve(__dirname, 'dist'),
-        }
+module.exports = [{
+    target: 'web',
+    entry: './src/index.ts',
+    mode: "production",
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    output: {
+        libraryTarget: 'umd',
+        globalObject: 'this',
+        library: 'providerSeed',
+        filename: 'provider-seed.js',
+        path: resolve(__dirname, './dist'),
     }
-];
+}];
